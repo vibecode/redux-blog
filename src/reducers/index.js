6c7c1e2646880b5de4463f42226ad06cc3144a53
vibecode <rumashkin@gmail.com>
2017-05-21 23:34:38 +0200

    implement posts reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,1 +1,8 @@
-export default () => null;
+import { combineReducers } from 'redux';
+import posts from './posts';
+
+const rootReducer = combineReducers({
+  posts
+});
+
+export default rootReducer;
